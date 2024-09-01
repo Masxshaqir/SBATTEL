f92abbcb75faf8eba5fa2a572037bd53e65b0d61
Marcus Shaqir <chkir44@gmail.com>
2024-09-01 16:40:01 +0000

    Add methods to randomly place ships on the grid and print the game board with proper formatting.

--- run.py
+++ run.py
@@ -14,3 +14,26 @@ class BaseBoard:
         self.num_ships = num_ships  
         self.grid = self.create_grid()  
         self.place_ships() 
+def create_grid(self):
+        """Creates and returns an empty grid of the specified size."""
+        return [[' ' for _ in range(self.grid_size)]  
+                for _ in range(self.grid_size)]
+def place_ships(self):
+        """Randomly places the specified number of ships on the grid."""
+        for _ in range(self.num_ships):  
+            row = random.randint(0, self.grid_size - 1)  
+            col = random.randint(0, self.grid_size - 1)  
+            while self.grid[row][col] == 'S':  
+                row = random.randint(0, self.grid_size - 1)  
+                col = random.randint(0, self.grid_size - 1)  
+            self.grid[row][col] = 'S'  
+
+    def print_board(self):
+        """Prints the current state of the grid."""
+        print("   " + " ".join([str(i) for i in range(len(self.grid))]))  
+        print("  " + "--" * len(self.grid))  
+        row_number = 0
+        for row in self.grid:  
+            print("%d |%s|" % (row_number, "|".join(row)))  
+            row_number += 1
+        print("\n")  
